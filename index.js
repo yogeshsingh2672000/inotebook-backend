0a695f4e6a5d4c5643f731423cbccaaa963054f5
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-08-31 16:57:22 +0530

    initial setup models, and routes

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+// Availabel routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
