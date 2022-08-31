4f79fe2a884baef7d65f6257a3616b2e292ddf06
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-08-31 18:44:29 +0530

    added hashing in password using bcrypt.js and generated auth token using jwtwebtoken

--- models/User.js
+++ models/User.js
@@ -21,5 +21,4 @@ const UserSchema = new Schema({
   },
 });
 const User = mongoose.model("user", UserSchema);
-User.createCollection();
 module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,14 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
-// Create user using: POST "/api/auth". does'nt require authentication
+const JWT_SECRET = "ThisIsUsedToSignTheJwtToken";
+
+// Create user using: POST "/api/auth/createuser". no login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Name must be 2 character long").isLength({
       min: 3,
@@ -13,24 +17,42 @@ router.post(
     body("email", "Please enter a valid Email").isEmail(),
     body("password", "Password must be 5 character long").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err.message);
-        res.json({
-          error: "Please enter a unique email",
-          message: err.message,
+    try {
+      // validating user already exist or not
+      let user = await User.findOne({ email: req.body.email });
+
+      // if user exist the return err
+      if (user != null) {
+        return res.json({
+          error: `user with ${user.email} email already exist`,
         });
+      }
+      const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password, salt);
+      //creating new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
       });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const atuhToken = jwt.sign(data, JWT_SECRET);
+      res.json({ atuhToken });
+      //   res.json({ user });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
 );
 
